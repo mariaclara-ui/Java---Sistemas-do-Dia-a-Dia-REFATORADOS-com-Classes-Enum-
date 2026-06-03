61e6103cdee7c19ca39b24b2a7db909d92ab15f0
mariaclara-ui <Mariaclaraduartedeniz@gmail.com>
2026-06-03 15:34:45 -0300

    Delete Att_01 directory

--- Att_01/Entidades/enums/Status.java
+++
@@ -1,8 +0,0 @@
-package Java_criacao_app.Atividade_Sistemas_dia_a_dia.Att_01.Entidades.enums;
-
-public enum Status {
-    QUANTIDADE_DE_MULTAS_RECEBIDAS,
-    QUANTIDADE_DE_LIVROS_RECEBIDOS,
-    PROCURANDO_LIVRO,
-    LIVRO_ADIQUIRIDO_COM_SUCESSO;
-}
