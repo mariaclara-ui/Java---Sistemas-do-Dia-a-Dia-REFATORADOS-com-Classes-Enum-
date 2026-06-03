f638d156d191c7ae43a788cc4965189a433e12d0
mariaclara-ui <Mariaclaraduartedeniz@gmail.com>
2026-06-03 15:36:21 -0300

    Delete Atividade_dia_a_dia01 directory

--- Atividade_dia_a_dia01/entidades/enums/Status.java
+++
@@ -1,8 +0,0 @@
-package Atividade_dia_a_dia01.entidades.enums;
-
-public enum Status {
-    QUANTIDADE_DE_MULTAS_RECEBIDAS,
-    QUANTIDADE_DE_LIVROS_RECEBIDOS,
-    PROCURANDO_LIVRO,
-    LIVRO_ADIQUIRIDO_COM_SUCESSO;
-}
