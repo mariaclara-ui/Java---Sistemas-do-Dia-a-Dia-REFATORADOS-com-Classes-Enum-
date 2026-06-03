138917e3d3b71cb64e900bb2f33409c5d617f0e8
mariaclara-ui <Mariaclaraduartedeniz@gmail.com>
2026-06-03 15:35:42 -0300

    Add files via upload

---
+++ Atividade_dia_a_dia01/entidades/Matricula.java
@@ -0,0 +1,31 @@
+package Atividade_dia_a_dia01.entidades;
+
+public class Matricula {
+    private String numeroMatricula;
+    private int multas;
+
+    public Matricula(String numeroMatricula, int multas){
+        this.numeroMatricula=numeroMatricula;
+        this.multas=multas;
+    }
+    public void setNumeroMatricula(String numeroMatricula) {
+        this.numeroMatricula = numeroMatricula;
+    }
+    public String getNumeroMatricula() {
+        return numeroMatricula;
+    }
+    public void setMultas(int multas) {
+        this.multas = multas;
+    }
+    public int getMultas() {
+        return multas;
+    }
+
+    @Override
+    public String toString() {
+        return "Matricula{" +
+                "numeroMatricula='" + numeroMatricula +
+                ", multas=" + multas ;
+    }
+}
+
---
+++ Atividade_dia_a_dia01/entidades/enums/Status.java
@@ -0,0 +1,8 @@
+package Atividade_dia_a_dia01.entidades.enums;
+
+public enum Status {
+    QUANTIDADE_DE_MULTAS_RECEBIDAS,
+    QUANTIDADE_DE_LIVROS_RECEBIDOS,
+    PROCURANDO_LIVRO,
+    LIVRO_ADIQUIRIDO_COM_SUCESSO;
+}
